2b8a584840cf133c9256f92565cbdc7455562251
Tatasha <tartarshia@gmail.com>
2018-08-22 17:29:44 -0400

    替换了原生/原版隐藏，用localstorage取代了chrome.storgae.local

--- stage1st_tailor/thread_list.js
+++ stage1st_tailor/thread_list.js
@@ -1,6 +1,6 @@
 chrome.runtime.sendMessage({ loc: "forum" });
 
-console.log($("#threadlisttableid > tbody"));
+// console.log($("#threadlisttableid > tbody"));
 
 var pattern = /thread_([0-9]+)/;
 var tid_list = new Array();
@@ -8,9 +8,13 @@ var tid_hidden_list = new Array();
 var tid_dim_list = new Array();
 
 var tid_map = {};
+
+
+$("[title='隐藏置顶帖']").remove();
+
 $("#threadlisttableid > tbody").each(function(p){
-  console.log($( this ).attr("id"));
-  console.log(pattern.exec($( this ).attr("id")));
+  // console.log($( this ).attr("id"));
+  // console.log(pattern.exec($( this ).attr("id")));
   if (pattern.exec($( this ).attr("id")) != null) {
     var tid = pattern.exec($( this ).attr("id"))[1];
     tid_list.push(tid);
@@ -19,22 +23,30 @@ $("#threadlisttableid > tbody").each(function(p){
     tid_map[tid] = $( this );
 
     $( this ).find("> tr > th")
-      .append('<span><input type="checkbox" id="hidden_' + tid + '">hide</span>');
-    $('#hidden_' + tid).change(function() { 
-      if (this.checked) {
-        var tmp = {};
-        tmp['hidden' + tid] = 1;
-        chrome.storage.local.set(tmp, function() {
-          if (chrome.runtime.lastError) {
-            console.log('set error' + chrome.runtime.lastError);
-            console.log(chrome.runtime.lastError);
-          } else {
-            console.log('hide: ' + tid);
-          }
-        });
+      // .append('<span><input type="checkbox" id="hidden_' + tid + '">hide</span>');
+      .append('<a id="hidden_' + tid + '" class="showhide y" title="隐藏">Hide</a>').click(function() {
+        var a = [];
+        a = JSON.parse(localStorage.getItem('hideThreadList'));
+        a.push(tid);
+        localStorage.setItem('hideThreadList', JSON.stringify(a));
         tid_map[tid].css("display", "none");
-      }
-    });
+      });
+      
+    // $('#hidden_' + tid).change(function() { 
+    //   if (this.checked) {
+    //     var tmp = {};
+    //     tmp['hidden' + tid] = 1;
+    //     chrome.storage.local.set(tmp, function() {
+    //       if (chrome.runtime.lastError) {
+    //         // console.log('set error' + chrome.runtime.lastError);
+    //         // console.log(chrome.runtime.lastError);
+    //       } else {
+    //         // console.log('hide: ' + tid);
+    //       }
+    //     });
+    //     tid_map[tid].css("display", "none");
+    //   }
+    // });
 
     $( this ).find("> tr > th")
       .append('<span><input type="checkbox" id="dim_' + tid + '">dim</span>');
@@ -44,10 +56,10 @@ $("#threadlisttableid > tbody").each(function(p){
         tmp['dim' + tid] = 1;
         chrome.storage.local.set(tmp, function() {
           if (chrome.runtime.lastError) {
-            console.log('set error' + chrome.runtime.lastError);
-            console.log(chrome.runtime.lastError);
+            // console.log('set error' + chrome.runtime.lastError);
+            // console.log(chrome.runtime.lastError);
           } else {
-            console.log('dim: ' + tid);
+            // console.log('dim: ' + tid);
           }
         });
         tid_map[tid].find("> tr > th > a.s.xst").css("color", "#f6f7eb");
@@ -56,16 +68,24 @@ $("#threadlisttableid > tbody").each(function(p){
   }
 })
 
-console.log(tid_list);
-console.log(tid_map);
+$("[title='查看更新']").each(function() {
+  $(this).parent().append('<span id="clearstickthread" style=""><span class="pipe">|</span><a href="javascript:;" class="xi2" title="toogle to display">显示隐藏</a></span>').find('[title="toogle to display"]').click(function() {
+    localStorage.hideThreadList='[]';
+  });
+});
+
+
+
+// console.log(tid_list);
+// console.log(tid_map);
 
 chrome.storage.local.get(tid_list, function(items) {
-  console.log('getting history: ');
-  console.log(items);
-  console.log(chrome.runtime.lastError);
+  // console.log('getting history: ');
+  // console.log(items);
+  // console.log(chrome.runtime.lastError);
   for(i in items) {
-    console.log(i);
-    console.log(tid_map[i]);
+    // console.log(i);
+    // console.log(tid_map[i]);
     console.log(tid_map[i].find("> tr > th")
       .append('<span"><a style="color:red;" href="https://bbs.saraba1st.com/2b/forum.php?mod=viewthread&page=' 
         + items[i].page + '&tid='
@@ -75,28 +95,35 @@ chrome.storage.local.get(tid_list, function(items) {
   }
 });
 
-chrome.storage.local.get(tid_hidden_list, function(items) {
-  console.log('getting hidden');
-  console.log(items);
-  console.log(chrome.runtime.lastError);
-  for(i in items) {
-    console.log(i.substring(6));
-    var tid = i.substring(6);
-    console.log(tid_map[tid]);
-    tid_map[tid].css("display", "none");
-  }
-});
+// chrome.storage.local.get(tid_hidden_list, function(items) {
+//   // console.log('getting hidden');
+//   // console.log(items);
+//   // console.log(chrome.runtime.lastError);
+//   for(i in items) {
+//     // console.log(i.substring(6));
+//     var tid = i.substring(6);
+//     // console.log(tid_map[tid]);
+//     tid_map[tid].css("display", "none");
+//   }
+// });
+
+var hideThreadList = JSON.parse(localStorage.getItem('hideThreadList'));
+for(thread in hideThreadList) {
+  var tid = hideThreadList[thread];
+  tid_map[tid].css("display", "none");
+}
+
 
 
 chrome.storage.local.get(tid_dim_list, function(items) {
-  console.log('getting dim');
-  console.log(items);
+  // console.log('getting dim');
+  // console.log(items);
   if (chrome.runtime.lastError) {
-    console.log(chrome.runtime.lastError);
+    // console.log(chrome.runtime.lastError);
   }
   for(i in items) {
     var tid = i.substring(3);
-    console.log('dim' + tid);
+    // console.log('dim' + tid);
     tid_map[tid].find("> tr > th > a.s.xst").css("color", "#f6f7eb");  // todo: check box 状态设置
   }
 });
